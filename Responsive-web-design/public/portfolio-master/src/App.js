2b8f80ca21650108daea87f40ad2849ad7acc5ee
Hussein-Mourad <husseinmouradkassem9901@gmail.com>
2020-12-29 04:39:03 +0200

    Started working on portfolio

---
+++ Responsive-web-design/public/portfolio-master/src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+function App() {
+  return (
+    <div className="m-10 p-5 shadow-lg w-4/5 rounded-xl flex">
+
+      <img src="https://picsum.photos/200" className="rounded-xl mr-4" alt="random img"></img>
+      <h2 className="text-gray-600 text-2xl" >Hussein Mourad</h2>
+    </div>
+  );
+}
+
+export default App;
