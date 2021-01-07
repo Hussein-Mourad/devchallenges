4dfe5d415290a253000f2e155922fdb9d42b60ea
Hussein-Mourad <husseinmouradkassem9901@gmail.com>
2021-01-08 00:06:04 +0200

    Adding StoryBook

--- Responsive-web-design/public/input-component-master/src/App.js
+++ Responsive-web-design/public/input-component-master/src/App.js
@@ -4,8 +4,34 @@ import Input from "./components/Input.js";
 
 function App() {
   return (
-    <div className="m-10">
-      <Input error/>
+    <div className="mx-16 my-8">
+      <h1>Regular</h1>
+      <Input placeholder="Placeholder" />
+      <h1>Error</h1>
+      <Input placeholder="Placeholder" error />
+      <h1>Disabled</h1>
+      <Input placeholder="Placeholder" disabled />
+      <h1>helperText="Some Text"</h1>
+      <Input placeholder="Placeholder" helperText="Some Text" />
+      <h1>helperText="Some Text" error </h1>
+      <Input placeholder="Placeholder" helperText="Some Text" error />
+      <h1>startIcon="phone"</h1>
+      <Input placeholder="Placeholder" startIcon="phone" />
+      <h1>startIcon="phone" error</h1>
+      <Input placeholder="Placeholder" startIcon="phone" error />
+      <h1>endIcon="lock"</h1>
+      <Input placeholder="Placeholder" endIcon="lock" />
+      <h1>endIcon="lock" error</h1>
+      <Input placeholder="Placeholder" endIcon="lock" error />
+      <h1>value="Text" </h1>
+      <Input placeholder="Placeholder" value="Text" />
+      <h1>size="sm" </h1>
+      <Input placeholder="Placeholder" size="sm" />
+      <h1>fullWidth</h1>
+      <Input placeholder="Placeholder" fullWidth />
+      <h1>nultiline</h1>
+      <Input placeholder="Placeholder" multiline rows="4" />
+      <Input placeholder="Placeholder" multiline rows="4" error/>
     </div>
   );
 }
--- Responsive-web-design/public/input-component-master/src/components/Input.js
+++ Responsive-web-design/public/input-component-master/src/components/Input.js
@@ -1,51 +1,188 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Icon from "@material-ui/core/Icon";
+import PropTypes from "prop-types";
 
-function Input({ error }) {
-  const [color, setColor] = useState(error ? "red-400" : "gray-700");
-  function onFocus() {
-    setColor("blue-400");
-  }
-  function onBlur() {
-    setColor("gray-700");
-  }
+const InputBase = (props) => {
   return (
-    <div>
-      <div className="my-4 relative text-gray-700 max-w-min">
-        <span
-          className="absolute top-1/2 transform -translate-y-1/2  left-3 mt-1 focus:outline-none"
-          tabIndex="0"
-        >
-          {/* <Icon className={`text-${color}`}>phone</Icon> */}
-        </span>
-        <div className="flex flex-col-reverse">
-          <input
-            type="text"
-            placeholder="Placeholder"
-            className="input input--error"
-            onFocus={onFocus}
-            onBlur={onBlur}
-          />
-          <label className="input-label input-label--error">Label</label>
-        </div>
-        <small className="block text-xs text-gray-400">Some text</small>
+    <div
+      className={`${props.className} my-4 relative text-gray-700 ${
+        props.fullWidth ? "w-full" : "max-w-min"
+      }`}
+    >
+      <span
+        className={`absolute top-1/2 ${
+          props.endIcon ? "right-3" : "left-3"
+        } transform -translate-y-1/2 mt-3 focus:outline-none 
+           `}
+      >
+        <Icon className={`text-${props.color}`}>{props.startIcon}</Icon>
+        <Icon className={`text-${props.color}`}>{props.endIcon}</Icon>
+      </span>
+
+      <div className="flex flex-col-reverse">
+        <input
+          type="text"
+          placeholder={props.placeholder}
+          className={props.setInputStyle()}
+          onFocus={props.onFocus}
+          onBlur={props.onBlur}
+          onMouseEnter={props.onMouseEnter}
+          onMouseLeave={props.onMouseLeave}
+          disabled={props.disabled}
+          value={props.value}
+        />
+        <label className={props.setLabelStyle()}>Label</label>
       </div>
 
-      <div className="my-3">
-        <label className="block text-sm text-gray-700 focus:text-blue-400">
-          Label
-        </label>
-        <textarea
-          type="text"
-          className="my-1 pl-3 pr-6 pt-4 border border-gray-500 rounded-lg placeholder-gray-500 hover:border-gray-900 focus:outline-none focus:border-blue-400 resize-none"
-          placeholder="Placeholder"
-          cols="22"
-          rows="4"
-        ></textarea>
-        <small className="block text-xs text-gray-400">Some text</small>
-      </div>
+      <small
+        className={`block text-xs ${
+          props.error ? "text-red-400" : "text-gray-400"
+        }`}
+      >
+        {props.helperText}
+      </small>
     </div>
   );
+};
+const InputMultiline = (props) => {
+  return (
+    <div
+      className={`${props.className} my-3 flex flex-col-reverse ${
+        props.fullWidth ? "w-full" : "max-w-min"
+      }`}
+    >
+      <small className="block text-xs text-gray-400">{props.helperText}</small>
+      <textarea
+        type="text"
+        className={`input my-1 pl-3 pt-4 border rounded-lg placeholder-gray-500 hover:border-gray-900 focus:outline-none  resize-none ${
+          props.error
+            ? "border-red-400 focus:border-red-400"
+            : "border-gray-500 focus:border-blue-400"
+        }`}
+        placeholder={props.placeholder}
+        cols={props.cols}
+        rows={props.rows}
+      ></textarea>
+      <label className={props.setLabelStyle()}>Label</label>
+    </div>
+  );
+};
+
+function Input({
+  startIcon,
+  endIcon,
+  helperText,
+  value,
+  placeholder,
+  size,
+  fullWidth,
+  multiline,
+  rows,
+  cols,
+  error,
+  disabled,
+  className,
+}) {
+  const [color, setColor] = useState(error ? "red-400" : "gray-700");
+  function onFocus() {
+    error ? setColor("red-400") : setColor("blue-400");
+  }
+  function onBlur() {
+    error ? setColor("red-400") : setColor("gray-700");
+  }
+  function onMouseEnter() {
+    setColor("gray-700");
+  }
+  function onMouseLeave() {
+    error ? setColor("red-400") : setColor("gray-700");
+  }
+
+  function setInputStyle() {
+    var sizeStyle = "py-4";
+    var padding = `${sizeStyle} pl-3`;
+    var border =
+      "border border-gray-500 hover:border-gray-900 focus:border-blue-400";
+    if (size === "sm") {
+      sizeStyle = "py-2";
+    }
+    if (startIcon) {
+      padding = `${sizeStyle} pl-10`;
+    } else if (endIcon) {
+      padding = `${sizeStyle} pr-10 pl-3`;
+    }
+
+    if (error) {
+      border =
+        "border border-red-400 hover:border-gray-900 focus:border-red-400";
+    }
+
+    if (disabled) {
+      border = "border border-gray-200 cursor-not-allowed";
+    }
+    var inputStyle = `input my-1 ${padding} ${border} rounded-lg text-gray-900  focus:outline-none`;
+    return inputStyle;
+  }
+
+  function setLabelStyle() {
+    var type = "default";
+    if (error) {
+      type = "error";
+    }
+    var labelStyle = `input-label--${type} block text-sm`;
+    return labelStyle;
+  }
+  const props = {
+    startIcon,
+    endIcon,
+    helperText,
+    value,
+    placeholder,
+    size,
+    fullWidth,
+    multiline,
+    rows,
+    cols,
+    error,
+    disabled,
+    color,
+    className,
+    onFocus,
+    onBlur,
+    onMouseEnter,
+    onMouseLeave,
+    setInputStyle,
+    setLabelStyle,
+  };
+  if (multiline) {
+    return <InputMultiline {...props} />;
+  } else {
+    return <InputBase {...props} />;
+  }
 }
+
+Input.propTypes = {
+  startIcon: PropTypes.string,
+  endIcon: PropTypes.string,
+  helperText: PropTypes.string,
+  value: PropTypes.string,
+  size: PropTypes.string,
+  fullWidth: PropTypes.bool,
+  multiline: PropTypes.bool,
+  rows: PropTypes.number,
+  cols: PropTypes.number,
+  error: PropTypes.bool,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+Input.defaultProps = {
+  size: "md",
+  fullWidth: false,
+  multiline: false,
+  rows: 30,
+  cols: 4,
+  error: false,
+  disabled: false,
+};
 
 export default Input;
