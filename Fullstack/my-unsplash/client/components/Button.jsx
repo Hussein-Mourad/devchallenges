6e7687769ba19d31582ab5504ed61eddc37cbf04
Hussein-Mourad <husseinmouradkassem9901@gmail.com>
2021-03-28 01:08:12 +0200

    UI is fine && working on server

--- Fullstack/my-unsplash/client/components/Button.jsx
+++ Fullstack/my-unsplash/client/components/Button.jsx
@@ -1,3 +1,23 @@
-export default function Button({ className, children }) {
-    return <button className= " px-4 py-2 border bg-green-600">{children}</button>;
+export default function Button({ className, children, color, onClick }) {
+    const getColor = () => {
+        if (color === "primary") {
+            return "bg-green-500 active:bg-green-600 text-white focus:ring focus:ring-green-200 ";
+        } else if (color === "danger") {
+            return "bg-red-500 active:bg-red-600 text-gray-100 focus:ring focus:ring-red-200 ";
+        } else {
+            return "text-gray-600 focus:ring-2 focus:ring-gray-500 ";
+        }
+    };
+    return (
+        <button
+            className={
+                "px-3 py-2 rounded-lg font-medium hover:opacity-95 focus:outline-none " +
+                getColor() +
+                className
+            }
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
 }
