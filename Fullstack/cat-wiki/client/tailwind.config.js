92efb2fa64f78b83371062d00b486b2c815c20b0
Hussein-Mourad <husseinmouradkassem9901@gmail.com>
2021-04-05 23:24:16 +0200

    Added three pages

--- Fullstack/cat-wiki/client/tailwind.config.js
+++ Fullstack/cat-wiki/client/tailwind.config.js
@@ -1,11 +1,18 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        hero: "#E3E1DC",
+        primary: "#291507",
+        secondary:"#4D270C"
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
